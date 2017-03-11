e9cf95a39b814080b5f0221fa849efa41829c502
Pratham03 <pratham.palkar@ymail.com>
2017-03-12 01:41:15 +0530

    Version 1 check-in
    Basic PNR search functionality checked in

---
+++ pnrBotInteractions.js
@@ -0,0 +1,68 @@
+var builder = require('botbuilder');
+var restify = require('restify');
+var pnrClient = require('./pnr-client.js');
+//Create a connector for the bot
+var connector = new builder.ChatConnector();
+//Creating the bot
+var pnrBot = new builder.UniversalBot(connector);
+
+var dialog = new builder.IntentDialog();
+
+dialog.matches(/^get status/i, [
+    function (session, args, next) {
+        if (session.message.text.toLowerCase() == 'get status') 
+        {
+            builder.Prompts.text(session, 'Which PNR number are you looking for?');
+        } 
+        else
+        {
+            var query = session.message.text.substring(11);
+            next({ response: query });
+        }
+    },
+    function (session, result, next) {
+        var query = result.response;
+        if (!query) {
+            session.endDialog('Request cancelled');
+        }
+        else
+        {
+            pnrClient.getPNRStatus(query, function(pnrStatus){
+                if(pnrStatus.response_code == "410")
+                {
+                    session.endDialog("PNR flushed or not generated");
+                }
+                else if(pnrStatus.response_code == "404")
+                {
+                    session.endDialog("Source not responding");
+                }
+                else
+                {
+                    var card = new builder.ThumbnailCard(session);
+                    card.title(pnrStatus.pnr);
+                    var text = '';
+                    text += pnrStatus.train_num + ' \n';
+                    text += 'Train Name:' + pnrStatus.train_name + ' \n';
+                    text += 'Date of Journey:' + pnrStatus.doj + ' \n';
+                    text +=  'From Station:' + pnrStatus.from_station.name + ' \n';
+                    text += 'To Station:' + pnrStatus.to_station.name + ' \n';
+                    for(var i = 0; i < pnrStatus.passengers.length; i++)
+                    {
+                        text += 'Passenger ' + i+1 + ' Status:' +  pnrStatus.passengers[i].current_status + ' \n';
+                        text += 'Passenger ' + i+1 + ' Status:' +  pnrStatus.passengers[i].coach_position + ' \n';
+                    }
+                    var message = new builder.Message(session).attachments([card]);
+                    session.send(message);
+
+                }
+
+            });
+        }
+    }
+    ]);
+pnrBot.dialog('/', dialog);
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+server.post('/api/messages', connector.listen());
